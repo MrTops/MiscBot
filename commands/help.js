e50580c706d8b5f5869d10e20917cf8fb941b23d
MrTops <43430084+MrTops@users.noreply.github.com>
2020-08-14 21:04:25 -0400

    added page mode

--- commands/help.js
+++ commands/help.js
@@ -71,10 +71,25 @@ exports.run = async (bot, message, args) => {
         }
         let pages = commandPages(bot);
         args[0] = parseInt(args[0]);
-        if (!pages[args[0]]) {
-            sendError(``)
+        if (!pages[args[0]-1]) {
+            sendError(`There is not a page \`\`${args[0]}\`\``, 8, message.channel);
             return true;
         }
+        const currentPageEmbed = new MessageEmbed()
+            .setColor(generateColor())
+            .setTimestamp()
+            .setAuthor(bot.user.username, bot.user.avatarURL(), 'https://github.com/MrTops/MiscBot')
+            .setTitle(`Page #${args[0]}/${pages.length}`);
+        let description = "";
+        pages[args[0]-1].forEach(command => {
+            let config = command.Config;
+            description += `Command: ${config.Command}\nDescription: ${config.Description}\nUsage: ${config.Usage}${config.Aliases ? `\nUsage: ${config.Aliases.join(', ')}` : ''}\n\n`;
+        });
+        currentPageEmbed.setDescription(description);
+        if (!(await message.author.send(currentPageEmbed).catch(err=>log))) {
+            sendError(`Was unable to send dm, are your dms off??`, 10, message.channel);
+            return true;
+        };
     }else {
         // just a check but shouldn't be called
         sendError('Was unable to parse argument, if this persists please contact a developer', 10, message.channel);
